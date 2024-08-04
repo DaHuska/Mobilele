520b466dc4e06715a58d4be0e42f6b5adda2b854
DaHuska <aboyanov@abv.bg>
2024-08-04 21:52:30 +0300

    Brands page commit

--- mobilele/src/main/java/org/softuni/mobilele/web/BrandController.java
+++ mobilele/src/main/java/org/softuni/mobilele/web/BrandController.java
@@ -1,14 +1,28 @@
 package org.softuni.mobilele.web;
 
+import org.softuni.mobilele.model.entity.Brand;
+import org.softuni.mobilele.service.BrandService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/brands")
 public class BrandController {
+    private final BrandService brandService;
+
+    public BrandController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
     @GetMapping("/all")
-    public String allBrands() {
+    public String allBrands(Model model) {
+        List<Brand> brands = brandService.getAllBrands();
+
+        model.addAttribute("brands", brands);
 
         return "brands";
     }
