92c90327a20fa8e2c70512992d07446a2636a534
DaHuska <aboyanov@abv.bg>
2024-08-21 19:48:12 +0300

    Remember Me Implementation

--- mobilele/src/main/java/org/softuni/mobilele/config/SecurityConfig.java
+++ mobilele/src/main/java/org/softuni/mobilele/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package org.softuni.mobilele.config;
 
+import org.softuni.mobilele.model.enums.RoleType;
 import org.softuni.mobilele.repository.UserRepository;
 import org.softuni.mobilele.service.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +15,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final String rememberMeKey;
+
+    public SecurityConfig(@Value("${mobilele.remember.me.key}") String rememberMeKey) {
+        this.rememberMeKey = rememberMeKey;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(
                 authorizeRequests -> authorizeRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/", "/users/login", "/users/register", "/users/login-error", "/brands/all", "/offers/add", "/offers/all").permitAll()
+                        .requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll()
+                        .requestMatchers("/offers/add", "/offers/all").permitAll()
+                        .requestMatchers("/brands/all").hasRole(RoleType.ADMIN.name())
                         .anyRequest().authenticated()
         ).formLogin(
                 loginForm -> loginForm
@@ -32,6 +43,13 @@ public class SecurityConfig {
                         .logoutUrl("/users/logout")
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
+        ).rememberMe(
+                rememberMe -> {
+                    rememberMe
+                            .key(rememberMeKey)
+                            .rememberMeParameter("rememberme")
+                            .rememberMeCookieName("rememberme");
+                }
         ).build();
     }
 
--- mobilele/src/main/java/org/softuni/mobilele/service/impl/UserDetailsServiceImpl.java
+++ mobilele/src/main/java/org/softuni/mobilele/service/impl/UserDetailsServiceImpl.java
@@ -1,7 +1,10 @@
 package org.softuni.mobilele.service.impl;
 
 import org.softuni.mobilele.model.entity.UserEntity;
+import org.softuni.mobilele.model.entity.UserRole;
 import org.softuni.mobilele.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,11 +29,21 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return userDetails;
     }
 
-    private UserDetails map(UserEntity userEntity) {
+    private static UserDetails map(UserEntity userEntity) {
         return User
                 .withUsername(userEntity.getUsername())
                 .password(userEntity.getPassword())
-                .authorities(List.of()) //TODO: Add roles
+                .authorities(
+                        userEntity
+                                .getRoles()
+                                .stream()
+                                .map(UserDetailsServiceImpl::map)
+                                .toList()
+                )
                 .build();
+    }
+
+    private static GrantedAuthority map(UserRole userRole) {
+        return new SimpleGrantedAuthority("ROLE_" + userRole.getRoleType().name());
     }
 }
